aca5faa0ddd8951c4d4871927828da66396ac01c
haniotak <h8XntUEBNae6>
2017-08-17 12:12:39 -0700

    add just a tiny bit of help

--- src/main/js/components/designHelpModal.js
+++ src/main/js/components/designHelpModal.js
@@ -31,69 +31,33 @@ export default class DesignHelpModal extends Component {
         return (
             <Modal bsSize='large' show={showModal} onHide={this.closeModal}>
                 <Modal.Header closeButton>
-                    <Modal.Title>New design help</Modal.Title>
+                    <Modal.Title>OSCARS help</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Panel>
                         <h2>TL; DR</h2>
                         <ListGroup>
-                            <ListGroupItem>Set a description, then set and lock the schedule,</ListGroupItem>
+                            <ListGroupItem>Set a description, then optionally change the schedule,</ListGroupItem>
                             <ListGroupItem>Add one fixture, decide its VLAN and bandwidth and lock it,</ListGroupItem>
                             <ListGroupItem>Add at least one more fixture, and lock it,</ListGroupItem>
                             <ListGroupItem>Click on any pipes, set their parameters, and lock them,</ListGroupItem>
                             <ListGroupItem>Click "Commit"</ListGroupItem>
                         </ListGroup>
                         <h2>Introduction</h2>
+                        <p>OSCARS is a system that automatically manages virtual circuits.</p>
+                        <p><i><b>Todo: write more help</b></i></p>
                         <h2>Concepts</h2>
                         <h3>Fixtures, junctions and pipes</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
+                        <p>We have taken these concepts from plumbing language - feedback is welcome</p>
+                        <p>Fixtures in plumbing are things such as sinks, faucets, etc - points where
+                            fluids enter or exit the system. We use "fixture" to represent the points where
+                            packets enter and exit our connection. The type of fixture we support currently
+                            represents a VLAN on a physical Ethernet port.</p>
+                        <p>Pipes connect things; anything that comes in on one end comes out the other. In our case
+                            pipes represent MPLS LSPs across our network from one device to another.</p>
+                        <p>Junctions are where things interconnect. In our case, a junction represents a
+                            physical device where our pipes and/or fixtures are interconnected. </p>
+
                     </Panel>
                 </Modal.Body>
                 <Modal.Footer>
