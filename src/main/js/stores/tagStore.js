9378abad8c7b4f4d7cbd3268d0c2a1dfc62ec00e
haniotak <h8XntUEBNae6>
2018-06-20 14:36:35 -0700

    add feature to load tag contents from google sheet

--- src/main/js/stores/tagStore.js
+++ src/main/js/stores/tagStore.js
@@ -9,27 +9,44 @@ class TagStore {
 
     @observable editTag = {
         contents: '',
-        category: ''
+        category: '',
+        source: '',
+        contentOptions: []
     };
 
     @observable editCtg = {
         category: '',
+        source: '',
         id: null
     };
 
     @action setEditedTagContents(contents) {
         this.editTag.contents = contents;
     }
-    @action setEditedTagCtg(ctg) {
-        this.editTag.category = ctg;
+    @action setEditedTagContentOptions(contentOptions) {
+        this.editTag.contentOptions = contentOptions;
+    }
+
+    @action setEditedTagCtg(category) {
+        this.store.categories.map(c => {
+            if (c.category === category) {
+                this.editTag.category = c.category;
+                this.editTag.source  = c.source;
+            }
+        });
     }
 
     @action setEditedCtg(ctg, id) {
         this.editCtg.category = ctg;
         this.editCtg.id = id;
     }
+    @action setEditedSource(src, id) {
+        this.editCtg.source = src;
+        this.editCtg.id = id;
+    }
 
-    @action updateCategories(ctg) {
+    @action
+    updateCategories(ctg) {
         this.store.categories = ctg;
     }
 }
